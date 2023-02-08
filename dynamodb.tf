08bf67106bf42925dbaf69cfd98331a75932dd7c
Ubuntu <ubuntu@ip-172-31-16-46.us-west-2.compute.internal>
2023-02-08 05:30:51 +0000

    test

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "tfstate_lock_db" {
     name            = "tfstate_lock_db"
-    read_capacity   = 20
+    read_capacity   = 10
     write_capacity  = 20
     hash_key        = "LockID"
 
